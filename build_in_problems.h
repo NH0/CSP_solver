fb12b9d2bc98ee0e9f172c175c6686e114f5e54c
valentine.h <valentine.hure@eleves.enpc.fr>
2021-01-18 17:48:00 +0100

    Update build_in_problems.h

--- build_in_problems.h
+++ build_in_problems.h
@@ -5,14 +5,13 @@
 
 class Knapsack : public CSP {
 public:
-    int dt,max,B_inf;
+    int dt,max;
     vector<int> v;
     vector<vector<int>> w;
     Knapsack(std::vector<int> values,std::vector<vector<int>> weights, std::vector<int> Wmax, int const Borne);
     Knapsack(Knapsack* knapsack,int B);
 
     int get_value_solution(std::vector<int> s);
-    int get_B_inf(){return B_inf;}
 };
 
 int value_criteria(int bin,vector<int> v);
